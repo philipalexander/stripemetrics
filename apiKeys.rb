415bc1c1157bc682bcba6aff40d954210ada4975
Philip Alexander <pjalexander2@gmail.com>
2014-08-11 20:36:07 -0600

    API keys for Plaid

--- apiKeys.rb
+++ apiKeys.rb
@@ -1,2 +1,9 @@
+
+Stripe API Key
 Test Secret Key: sk_test_4VPBvbfPQ44Ft2rvc7XOzm2X
 Test Published Key: pk_test_4VPB8DsMPp6mr2n7ktUo8cE7
+
+
+Plaid API Keys
+client_id :    53e546b4f0c53e324455b524
+secret :       hEUBw3zmT04H82oSnN3uim
