a0274ad247f615d67be5b81c2ac0469b87294824
Ky <kpalmer8@gmail.com>
2014-08-11 21:57:48 -0600

    Made a major pivot to the Plaid platform in order to reduce douche-baggery

---
+++ PlaidTest.rb
@@ -0,0 +1,28 @@
+require 'yaml'
+require 'plaid'
+
+keys = YAML.load_file("config.yml")
+
+
+Plaid.config do |config|
+	config.customer_id = keys["CUSTOMER_ID"]
+	config.secret = keys["SECRET"]
+end
+
+new_account = Plaid.call.add_account( "amex","plaid_test", "plaid_good","test@example.com")
+puts new_account[:code]
+
+
+#puts "Enter your wells username:"
+#username = gets
+#puts "Enter your wells password:"
+#password = gets
+#puts "Enter email to associate with account"
+#email = gets
+#
+#
+#`curl -X POST https://tartan.plaid.com/connect \
+#   -d client_id=#{client_id} \
+#	 -d secret=#{secret} \
+#   -d credentials='{
+#       "username":#{username}
--- apiKeys.rb
+++ apiKeys.rb
@@ -7,3 +7,7 @@ Test Published Key: pk_test_4VPB8DsMPp6mr2n7ktUo8cE7
 Plaid API Keys
 client_id :    53e546b4f0c53e324455b524
 secret :       hEUBw3zmT04H82oSnN3uim
+
+
+access_token for philip:
+	WyI1M2U1NDZiNGYwYzUzZTMyNDQ1NWI1MjQiLCI1M2U5ODJmNDUyM2Y0ZGJmNzI5YjQ5YTIiLCI1M2U5ODJmZTUyM2Y0ZGJmNzI5YjQ5YTMiXQ==	
